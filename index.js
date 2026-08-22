b85d8755a275f47dca8b70f010b6db66b3af511c
himasabry <36902869+himasabry@users.noreply.github.com>
2026-08-23 01:10:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -409,7 +409,7 @@ app.get("/dashboard", (req, res) => {
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Arabic:wght@400;500;600;700&family=IBM+Plex+Mono:wght@400;500;600&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@2.47.0/dist/tabler-icons.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tabler-icons/3.46.0/tabler-icons.min.css">
 
 <style>
 
