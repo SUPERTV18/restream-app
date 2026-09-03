6538c3d830f883d2985578b18fd8dd1d866fb72b
super tv <36902869+SUPERTV18@users.noreply.github.com>
2026-09-04 01:26:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -251,7 +251,7 @@ const channels = {
   ch1: {
     input: "https://163.ostv.info/krikar/krikar/652333?token=ShJcU0BbQQNHDgxcBwYDCVsBAwdTV1FYCVdTAQABBAtUCAAHCwZTCAAbGBpEF0dcWF1vWQIXXlIJWlcFVEgRR0JVRm1aV0EDRwgBCgRWDAkbHBJED1gBQwtTVQFQXQQKBwUHHhFDCl1HA1pNWw8ZG1xIRFUUWwUNbgYHQQwHVhALXkFeXx9BVgtmUF1aAltdGwoSAUQZRghCEkANCxFfXh0SVltHQQJNABsOVkIPWRUbU19FCEEWGBNYQH40Rh8QVEhAV11AClYLGw4aQxAXFRtZQ28UUBcVQwcDWgAWEQgTABYeEV4CQTpaW1ZZBlZNUF9eQ0QPRlATTkBaCgpaRl5Ca0JaV0EDC0xYVEo=",
     output: "rtmp://vsu.okcdn.ru/input/15037126680149_16572030782037_nwbfmzaoxm",
-    logo: "logo1.png",
+    logo: "",
     category: "رياضة",
     watchUrl: "https://super-tvlive.vercel.app/SUPERTV_1.m3u8",
     quality: "high"
